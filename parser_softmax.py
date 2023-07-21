010c8a92b9d3f0e133aad6d692144637fff8a2c3
AlexDarkVape <kalash23rusa@gmail.com>
2023-07-21 10:39:40 +0400

    Update parser_softmax.py

--- parser_softmax.py
+++ parser_softmax.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def transform_dataframe_to_dict(df):
+def transform_dataframe_to_dict(soft_file):
+    df = pd.read_csv(soft_file,sep='\t',header=None)
     column_name = 0
     result_dict = {}
     current_key = None
@@ -27,7 +28,6 @@ def transform_dataframe_to_dict(df):
                     result_dict[current_key][key_value[0][1:].strip()] = key_value[1].strip()  # remove "!" from key
 
     return result_dict
+
 # usage
-url = 'ftp.ncbi.nlm.nih.gov/geo/series/GSE113nnn/GSE113264/soft/GSE113264_family.soft.gz'
-df = pd.read_csv(url,sep='\t',header=None)
-result_dict = transform_dataframe_to_dict(df)
+result_dict = transform_dataframe_to_dict(url)
